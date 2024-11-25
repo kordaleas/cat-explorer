10a743f4053385a6e72318f10430fee3ef0df393
Alex Kord <kordaleas@hotmail.com>
2024-11-25 15:37:25 +0200

    feat: implement search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatCardComponent } from './components/cat-card/cat-card.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 import { CatService } from './services/cat.service';
 import { CatBreed } from './models/cat.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CatCardComponent, SearchBarComponent, ThemeToggleComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule, CatCardComponent, SearchBarComponent, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,14 +19,38 @@ export class AppComponent implements OnInit {
   title = 'cat-explorer';
 
   breeds: CatBreed[] = [];
+  filteredBreeds: CatBreed[] = [];
 
-  constructor(private catService: CatService) {}
+  constructor(private catService: CatService, @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
 
   ngOnInit() {
+    this.loadBreeds();
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkUrlParams();
+    }
+  }
+
+  loadBreeds() {
     this.catService.getBreeds().subscribe(breeds => {
       this.breeds = breeds;
+      this.filteredBreeds = breeds;
     });
   }
 
+  handleSearch(searchTerm: string) {
+    this.filteredBreeds = this.breeds.filter(breed =>
+      breed.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
 
+  private checkUrlParams() {
+    if (isPlatformBrowser(this.platformId)) {
+      const params = new URLSearchParams(window.location.search);
+      const searchTerm = params.get('search');
+      if (searchTerm) {
+        this.handleSearch(searchTerm);
+      }
+    }
+  }
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, Output, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  @Output() search = new EventEmitter<string>();
+  searchTerm = '';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const params = new URLSearchParams(window.location.search);
+      this.searchTerm = params.get('search') || '';
+      if (this.searchTerm) {
+        this.onSearch();
+      }
+    }
+  }
+
+  onSearch() {
+    this.search.emit(this.searchTerm);
+    if (isPlatformBrowser(this.platformId)) {
+      const url = new URL(window.location.href);
+      url.searchParams.set('search', this.searchTerm);
+      window.history.pushState({}, '', url);
+    }
+  }
 
 }
