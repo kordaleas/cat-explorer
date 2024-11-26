cda6f58ac7d1b52df5dc6ce223779a742aaeebd9
Alex Kord <kordaleas@hotmail.com>
2024-11-26 12:43:07 +0200

    feat: cache server responses

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, makeStateKey, TransferState } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, of, throwError } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { CatBreed, CatImage } from '../models/cat.interface';
 import { ApiError } from '../models/error.interface';
+import { CacheService } from './cache.service';
 
 const BREED_IMAGE_KEY = makeStateKey<Record<string, string>>('breedImages');
 
@@ -12,11 +13,19 @@ const BREED_IMAGE_KEY = makeStateKey<Record<string, string>>('breedImages');
 export class CatService {
   private apiUrl = 'https://api.thecatapi.com/v1';
 
-  constructor(private http: HttpClient, private transferState: TransferState
+  constructor(private http: HttpClient, private transferState: TransferState, private cacheService: CacheService
   ) { }
 
   getBreeds(): Observable<CatBreed[]> {
+    const cached = this.cacheService.get('breeds');
+    if (cached) {
+      return of(cached);
+    }
+
     return this.http.get<CatBreed[]>(`${this.apiUrl}/breeds`).pipe(
+      tap((breeds: CatBreed[]) => {
+        this.cacheService.set('breeds', breeds);
+      }),
       catchError((error: HttpErrorResponse) => {
         const apiError: ApiError = {
           statusCode: error.status,
@@ -34,7 +43,15 @@ export class CatService {
       return of([{ url: images[breedId] } as CatImage]);
     }
 
+    const cacheKey = `breed-image-${breedId}`;
+    const cached = this.cacheService.get(cacheKey);
+    if (cached) {
+      return of(cached);
+    }
+
+
     return this.http.get<CatImage[]>(`${this.apiUrl}/images/search?breed_ids=${breedId}`).pipe(
+      tap(images => this.cacheService.set(cacheKey, images)),
       catchError((error: HttpErrorResponse) => {
         const apiError: ApiError = {
           statusCode: error.status,
